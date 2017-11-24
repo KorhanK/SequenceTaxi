b468703662db6116c238226edb770dfc619792ef
pauline <pauline.feytel@gmail.com>
2017-11-24 09:59:59 +0100

    Add for deciding the number of the length of the sequence (between 3 to 10). Refactor code.

--- app/src/main/java/com/example/pauline/groupprojecttry/MainActivity.java
+++ app/src/main/java/com/example/pauline/groupprojecttry/MainActivity.java
@@ -4,38 +4,34 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Calendar;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView value1 = null;
-    TextView value2 = null;
-    TextView value3 = null;
-    TextView value4 = null;
+    TextView sequence = null;
+    Random randomInt;
 
     LinearLayout linearLayoutButtons;
 
-    Thread t2 = null;
+    ArrayAdapter<String> adapter;
+    Spinner spinner;
 
-    Integer int1 = 0;
-    Integer int2 = 0;
-    Integer int3 = 0;
-    Integer int4 = 0;
+    Thread thread2 = null;
 
-    String valueSave = "";
-    StringBuilder valueButton = new StringBuilder();
+    StringBuilder generatedSequence = new StringBuilder();
+    StringBuilder userSequence = new StringBuilder();
 
-    Integer[] arrayInt = {1, 2, 3, 4};
+    Integer[] nbSequence = {3, 4, 5, 6, 7, 8, 9, 10};
+
+    Integer lengthSequence = 0;
 
     int countClickButtons = 0;
 
@@ -43,65 +39,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void go(View view) {
-        RandomValueInt();
+        spinner = findViewById(R.id.spinner);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, this.nbSequence);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-        value1 = (TextView) findViewById(R.id.value1);
-        value2 = (TextView) findViewById(R.id.value2);
-        value3 = (TextView) findViewById(R.id.value3);
-        value4 = (TextView) findViewById(R.id.value4);
-        value1.setText(int1.toString());
-        value2.setText(int2.toString());
-        value3.setText(int3.toString());
-        value4.setText(int4.toString());
 
-        valueSave = int1.toString() + int2.toString() + int3.toString() + int4.toString();
-
-        Thread t = new Thread() {
-
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(4000);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            // TextView update
-                            value1.setText("");
-                            value2.setText("");
-                            value3.setText("");
-                            value4.setText("");
-
-                            valueButton.delete(0, valueButton.length());
-                            linearLayoutButtons = (LinearLayout) findViewById(R.id.buttons);
-                            linearLayoutButtons.setVisibility(View.VISIBLE);
-                        }
-                    });
-                } catch (InterruptedException e) {
-                }
-            }
-        };
-        t.start();
-
-        t2 = new Thread() {
-
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(10000);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            checkSequence();
-                        }
-                    });
-                } catch (InterruptedException e) {
-                }
-            }
-        };
-        t2.start();
 
         final Button button1 = findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
@@ -132,45 +76,100 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void RandomValueInt() {
-        Random randomInt = new Random();
-        int1 = randomInt.nextInt(arrayInt.length) + 1;
-        int2 = randomInt.nextInt(arrayInt.length) + 1;
-        int3 = randomInt.nextInt(arrayInt.length) + 1;
-        int4 = randomInt.nextInt(arrayInt.length) + 1;
+    public void go(View view) {
+        lengthSequence = (Integer) spinner.getItemAtPosition(spinner.getSelectedItemPosition());
+
+        randomValueInt(lengthSequence);
+
+        sequence = (TextView) findViewById(R.id.sequence);
+        sequence.setText(generatedSequence.toString());
+
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(4000);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            sequence.setText("");
+
+                            userSequence.delete(0, userSequence.length());
+
+                            linearLayoutButtons = (LinearLayout) findViewById(R.id.buttons);
+                            linearLayoutButtons.setVisibility(View.VISIBLE);
+                        }
+                    });
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        thread.start();
+
+        thread2 = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10000);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            checkSequence();
+                        }
+                    });
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        thread2.start();
+
+
+    }
+
+    public void randomValueInt(int nb) {
+        randomInt = new Random();
+        int value;
+        for (int i = 0; i < nb; i++) {
+            value = randomInt.nextInt(4) + 1;
+            generatedSequence.append(value);
+        }
+
     }
 
     public void checkSequence() {
-        if (countClickButtons == 4 || !t2.isAlive()) {
-            if (valueSave.equals(valueButton.toString())) {
-                AlertMessage("Right");
+        if (countClickButtons == lengthSequence || !thread2.isAlive()) {
+            if (generatedSequence.toString().equals(userSequence.toString())) {
+                alertMessage("Right");
             } else {
-                AlertMessage("Wrong");
+                alertMessage("Wrong");
             }
             linearLayoutButtons.setVisibility(View.INVISIBLE);
-            t2.interrupt();
+            thread2.interrupt();
+            generatedSequence.delete(0, lengthSequence);
             countClickButtons = 0;
         }
     }
 
-    public void AlertMessage(String message) {
+    public void alertMessage(String message) {
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert
-                .setMessage(message)
-                .setCancelable(false)
-                .setPositiveButton("Ok",new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,int id) {
-                        dialog.cancel();
-                    }
-                });
+        alert.setMessage(message);
+        alert.setCancelable(false);
+        alert.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int id) {
+                dialog.cancel();
+            }
+        });
         AlertDialog alertDialog = alert.create();
         alertDialog.show();
     }
 
-    public void onClickButton(String valueB) {
+    public void onClickButton(String valueClick) {
         countClickButtons++;
-        valueButton.append(valueB);
-        Toast.makeText(getApplicationContext(), valueButton.toString(), Toast.LENGTH_SHORT).show();
+        userSequence.append(valueClick);
+        Toast.makeText(getApplicationContext(), userSequence.toString(), Toast.LENGTH_SHORT).show();
         checkSequence();
     }
+
 }
