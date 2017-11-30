a8908eba847ee158ae18b0a6d5c163ddd72e55d3
Muhamed Alani <mohdalani7@gmail.com>
2017-11-30 11:30:30 +0100

    tutorial page and Activity done

---
+++ app/src/main/java/com/example/pauline/groupprojecttry/Tutorial.java
@@ -0,0 +1,31 @@
+package com.example.pauline.groupprojecttry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Tutorial extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+        textView=(TextView)findViewById(R.id.tutorial);
+    }
+
+
+
+    public void goBack(View view){
+
+        String but_text=((Button)view).getText().toString();
+
+    }
+
+
+
+
+
+}
